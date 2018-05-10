839285f0fb927818fca2f3c752c84119189e9f8d
Dom Wakeling <domwakeling@gmail.com>
2018-05-10 23:32:38 +0100

    Iniital commit for 'empty' app

---
+++ imports/ui/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from 'material-ui';
+
+const App = () => (
+    <Typography variant="display4">
+        App Name Goes Here
+    </Typography>
+);
+
+export default App;
